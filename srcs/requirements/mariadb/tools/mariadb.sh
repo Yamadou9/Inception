f64ed8fedf50d4583a19c20bba1ac0211d586a87
ydembele <ydembele@f2r6s13.paris.42.school>
2026-06-29 15:49:34 +0200

    fix mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -7,13 +7,16 @@ until mysqladmin ping --silent 2>/dev/null; do
     sleep 1
 done
 
-mysql -u root <<EOF
+# N'init que si la DB n'existe pas déjà
+if ! mysql -u root -e "USE \`${SQL_DATABASE}\`;" 2>/dev/null; then
+    mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
+fi
 
 mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
 
